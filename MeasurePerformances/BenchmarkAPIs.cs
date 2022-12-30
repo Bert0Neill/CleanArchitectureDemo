32dbffa1e97033b41f9107ed44e0aec60496fd3c
Bert O'Neill <albert.neill@optum.com>
2022-12-30 03:25:18 +0000

    New blazor pages

---
+++ MeasurePerformances/BenchmarkAPIs.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeasurePerformances
+{
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class BenchmarkAPIs
+    {
+        private static HttpClient _httpClient;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            //Write your initialization code here           
+        }
+
+        [Benchmark]
+        public void RetrieveAlbums()
+        {
+            //Write your code here
+
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7074/");
+
+            var response = _httpClient.GetAsync("api/RetrieveLatestAlbumsAsync");            
+        }
+        [Benchmark]
+        public void RetrieveArtists()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7074/");
+
+            var response = _httpClient.GetAsync("api/RetrieveMostActiveArtistsAsync");            
+        }
+    }
+}
---
+++ MeasurePerformances/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+using MeasurePerformances;
+
+BenchmarkAPIs benchmarks = new BenchmarkAPIs();
+benchmarks.RetrieveAlbums();
+
+var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+Console.WriteLine("Hello, World!");
